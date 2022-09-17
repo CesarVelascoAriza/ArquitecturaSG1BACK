ab0718e7fdf65faede990fa29e2efd54783fbecb
cesar velasco <caacesar.ariza@gmail.com>
2022-09-17 00:11:09 -0500

    Common de oficina
    se agrega el common de la oficina

---
+++ common-oficina/src/main/java/co/edu/ucentral/common/oficina/model/Departamento.java
@@ -0,0 +1,10 @@
+package co.edu.ucentral.common.oficina.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departamentos")
+public class Departamento {
+
+}
---
+++ common-oficina/src/main/java/co/edu/ucentral/common/oficina/model/Oficina.java
@@ -0,0 +1,10 @@
+package co.edu.ucentral.common.oficina.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "oficinas")
+public class Oficina {
+
+}
