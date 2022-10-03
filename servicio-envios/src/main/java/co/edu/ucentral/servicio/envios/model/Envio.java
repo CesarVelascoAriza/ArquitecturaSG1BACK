ceedd0fdef65028c6088c7ff75f67fa1b015d3e2
camilo957 <camilo957@hotmail.com>
2022-10-02 19:33:29 -0500

    Se añade el servicio envios

---
+++ servicio-envios/src/main/java/co/edu/ucentral/servicio/envios/model/Envio.java
@@ -0,0 +1,128 @@
+package co.edu.ucentral.servicio.envios.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import co.edu.ucentral.common.oficina.model.Ciudad;
+import co.edu.ucentral.commons.model.Tarifa;
+import co.edu.ucentral.commons.usuario.models.Usuario;
+
+@Entity
+@Table(name = "envios")
+public class Envio implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int id;
+	@Column(name = "fecha_creacion")
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date fechaCreacion;
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "usuario_emisor")
+	private Usuario usuarioEmisor;
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "usuario_receptor")
+	private Usuario usuarioReceptor;
+	@ManyToOne
+	@JoinColumn(name = "ciudad_origen")
+	private Ciudad ciudadOrigen;
+	@ManyToOne
+	@JoinColumn(name = "ciudad_destino")
+	private Ciudad ciudadDestino;
+	@ManyToOne
+	@JoinColumn(name = "tarifa_id")
+	private Tarifa tarifa;
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Tarifa tarifaCategoriaId;
+
+	@PrePersist
+	public void prePersist() {
+		fechaCreacion = new Date();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Usuario getUsuarioEmisor() {
+		return usuarioEmisor;
+	}
+
+	public void setUsuarioEmisor(Usuario usuarioEmisor) {
+		this.usuarioEmisor = usuarioEmisor;
+	}
+
+	public Usuario getUsuarioReceptor() {
+		return usuarioReceptor;
+	}
+
+	public void setUsuarioReceptor(Usuario usuarioReceptor) {
+		this.usuarioReceptor = usuarioReceptor;
+	}
+
+	public Ciudad getCiudadOrigen() {
+		return ciudadOrigen;
+	}
+
+	public void setCiudadOrigen(Ciudad ciudadOrigen) {
+		this.ciudadOrigen = ciudadOrigen;
+	}
+
+	public Ciudad getCiudadDestino() {
+		return ciudadDestino;
+	}
+
+	public void setCiudadDestino(Ciudad ciudadDestino) {
+		this.ciudadDestino = ciudadDestino;
+	}
+
+	public Tarifa getTarifa() {
+		return tarifa;
+	}
+
+	public void setTarifa(Tarifa tarifa) {
+		this.tarifa = tarifa;
+	}
+
+	public Tarifa getTarifaCategoriaId() {
+		return tarifaCategoriaId;
+	}
+
+	public void setTarifaCategoriaId(Tarifa tarifaCategoriaId) {
+		this.tarifaCategoriaId = tarifaCategoriaId;
+	}
+
+}
---
+++ servicio-envios/src/main/java/co/edu/ucentral/servicio/envios/service/EnvioService.java
@@ -0,0 +1,8 @@
+package co.edu.ucentral.servicio.envios.service;
+
+import co.edu.ucentral.commons.services.service.CommonsService;
+import co.edu.ucentral.servicio.envios.model.Envio;
+
+public interface EnvioService extends CommonsService<Envio>{
+
+}
