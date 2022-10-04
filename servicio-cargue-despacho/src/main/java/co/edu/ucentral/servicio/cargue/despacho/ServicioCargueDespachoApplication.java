f71e4b43037648f586d15223763eb8769e6714b2
cesar velasco <caacesar.ariza@gmail.com>
2022-10-03 23:38:53 -0500

    cambio en el servicio
    servicio cambia

--- servicio-cargue-despacho/src/main/java/co/edu/ucentral/servicio/cargue/despacho/ServicioCargueDespachoApplication.java
+++ servicio-cargue-despacho/src/main/java/co/edu/ucentral/servicio/cargue/despacho/ServicioCargueDespachoApplication.java
@@ -2,8 +2,13 @@ package co.edu.ucentral.servicio.cargue.despacho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({
+	"co.edu.ucentral.common.despacho.model",
+	"co.edu.ucentral.servicio.cargue.despacho"
+})
 public class ServicioCargueDespachoApplication {
 
 	public static void main(String[] args) {
